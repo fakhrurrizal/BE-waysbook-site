5d0db7959c75113afd31d05321004d12e9cf15ed
Muhammad Fakhrur Rizal <fakhrurrizal10@gmail.com>
2022-12-14 20:39:31 +0700

    deployy

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 
 func main() {
 	errEnv := godotenv.Load()
-		if errEnv != nil {
-			panic("Failed to load env file")
-		}
-	
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
 	mysql.DatabaseInit()
 
 	database.RunMigration()
@@ -30,11 +30,11 @@ func main() {
 	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 
 	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
-	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT","HEAD" ,"OPTIONS", "PATCH", "DELETE"})
+	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
 	var port = os.Getenv("PORT")
 
-	fmt.Println("server running localhost:" +port)
-	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	fmt.Println("server running localhost:" + port)
+	http.ListenAndServe(""+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
